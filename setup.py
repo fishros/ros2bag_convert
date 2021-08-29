1d0848ea11d9fdde14931358142842aec646fde3
sangxin <sangxin2014@sina.com>
2021-08-30 02:17:17 +0800

    修改setup.sh

--- setup.py
+++ setup.py
@@ -5,12 +5,8 @@ package_name = 'ros2bag_convert'
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=["ros2bag_convert.ros2bag_convert"],
+    # data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]), ('share/' + package_name, ['package.xml']), ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='fishros',
